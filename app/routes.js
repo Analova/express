f34a76e920654bdc8ddf32bfff434533b3e562b0
Ana Nikolova <ani.nik19@gmail.com>
2020-06-09 09:58:05 +0200

    pug template engine with express

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,19 @@
 express = require("express");
 router = express.Router();
 
-router.get("/", (req, res) => res.send("<h1>Welcome to Billy Page</h1>"));
+router.get("/", (req, res) => {
+  return res.render("../assets/views/homepage.pug");
+});
+
+router.get("/pug", (req, res) => {
+  return res.render("../assets/views/testing.pug", {
+    username: "skyscaner",
+    fname: "Billy",
+    lname: "Lorenz",
+    loggedIn: true,
+    friends: ["Jane", "Billy", "Laura", "Jonny"],
+  });
+});
 
 router.get("/about/:username/:state", function (req, res) {
   const user = req.params;
--- app/server.js
+++ app/server.js
@@ -2,7 +2,12 @@ express = require("express");
 const app = express();
 const port = 3000;
 
+// Routes
 const routes = require("./routes.js");
+//Static Files Folder
+app.use(express.static("public"));
+//Tamplate engine
+app.set("view engine", "pug");
 
 app.use("/", routes);
 
